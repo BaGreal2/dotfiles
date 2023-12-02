7927671c23a6af03f7f72d08e8b3cdbd31a447c0
BaGreal2 <artemonik17@gmail.com>
2023-12-02 13:00:47 +0100

    moved from packer to lazy

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ end
 
 alias vim=nvim
 alias ls="lsd --group-dirs first"
+# fish_vi_key_bindings
 
 # pnpm
 set -gx PNPM_HOME "/Users/macbook/Library/pnpm"
@@ -11,9 +12,32 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# fzf
+# function fzf --wraps="fzf"
+#     set -Ux FZF_DEFAULT_OPTS "
+#     --color=fg:#908caa,bg:#191724,hl:#ebbcba
+#     --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+#     --color=border:#403d52,header:#31748f,gutter:#191724
+#     --color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+#     --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
+#   "
+#     command fzf
+# end
+set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+fzf_configure_bindings --directory=\cf --history=\cy
+#fzf end
+
 set --export PATH /usr/local/opt/llvm/bin $PATH
 set --export LDFLAGS -L/usr/local/opt/llvm/lib
 
 set --export PATH $HOME/.cargo/bin $PATH
+set -Ux BAT_THEME TwoDark
+
+set --export LC_ALL en_US.UTF-8  
+set --export LANG en_US.UTF-8
+
+set -gx $EDITOR "nvim"
 
 set fish_greeting ""
+
--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -16,7 +16,10 @@ null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
+      diagnostics_format = '[eslint] #{m}\n(#{c})',
+      condition = function(utils)
+        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" })
+      end,
     }),
     null_ls.builtins.diagnostics.fish
   },
