c5b926976a24034ce8573e88a5668a5fb42095a2
BaGreal2 <artemonik17@gmail.com>
2024-10-08 00:54:24 +0200

    [nvim] fix telescope delete bind

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -33,6 +33,7 @@ telescope.setup {
           -- your custom normal mode mappings
           ["N"] = fb_actions.create,
           ["h"] = fb_actions.goto_parent_dir,
+          ["d"] = fb_actions.remove,
           ["m"] = fb_actions.toggle_hidden,
           ["/"] = function()
             vim.cmd('startinsert')
