71c8a265bad982a9ad88e93e77b20abf04b94cd7
BaGreal2 <artemonik17@gmail.com>
2024-12-04 00:38:55 +0100

    [nvim] git tools config

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,4 +1,19 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  'dinhhuy258/git.nvim',
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+      vim.keymap.set('n', '<leader>gb', '<Cmd>Gitsigns blame_line<CR>', {})
+    end
+  },
+  {
+    'dinhhuy258/git.nvim',
+    config = function()
+      require('git').setup({
+        keymaps = {
+          blame = ""
+        }
+      })
+    end
+  }
 }
