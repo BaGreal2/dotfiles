6a5808a01189a5f520246e456f6c311fa8bd7ff5
BaGreal2 <artemonik17@gmail.com>
2024-12-04 00:38:46 +0100

    [nvim] lazy keybinds for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,87 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = "Telescope",
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+    keys = {
+      {
+        "sf",
+        function()
+          local function telescope_buffer_dir()
+            return vim.fn.expand('%:p:h')
+          end
+
+          require('telescope').extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            disable_devicons = true,
+            layout_config = { height = 40 }
+          })
+        end,
+        mode = { "n" }
+      },
+      {
+        ";f",
+        function()
+          require("telescope.builtin")
+              .find_files({
+                no_ignore = false,
+                hidden = true,
+                layout_config = { width = 0.5 },
+                preview = {
+                  hide_on_startup = true
+                }
+              })
+        end,
+        mode = { "n" }
+      },
+      {
+        ";r",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        mode = { "n" }
+      },
+      {
+        "\\\\",
+        function()
+          require("telescope.builtin").buffers({
+            sort_mru = true,
+            sort_lastused = true,
+            initial_mode = "normal",
+          })
+        end,
+        mode = { "n" }
+      },
+      {
+        ";t",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        mode = { "n" }
+      },
+      {
+        ";;",
+        function()
+          require("telescope.builtin").resume()
+        end,
+        mode = { "n" }
+      },
+      {
+        ";e",
+        function()
+          require("telescope.builtin").diagnostics()
+        end,
+        mode = { "n" }
+      }
+    },
     config = function()
       local actions = require('telescope.actions')
-      local builtin = require("telescope.builtin")
-
-      local function telescope_buffer_dir()
-        return vim.fn.expand('%:p:h')
-      end
 
       local fb_actions = require "telescope".extensions.file_browser.actions
 
@@ -58,56 +131,6 @@ return {
       }
 
       require('telescope').load_extension("file_browser")
-
-      vim.keymap.set("n", "sf", function()
-        require('telescope').extensions.file_browser.file_browser({
-          path = "%:p:h",
-          cwd = telescope_buffer_dir(),
-          respect_gitignore = false,
-          hidden = true,
-          grouped = true,
-          previewer = false,
-          initial_mode = "normal",
-          disable_devicons = true,
-          layout_config = { height = 40 }
-        })
-      end)
-
-      -- keymaps
-      vim.keymap.set('n', ';f',
-        function()
-          builtin.find_files({
-            no_ignore = false,
-            hidden = true,
-            layout_config = { width = 0.5 },
-            preview = {
-              hide_on_startup = true
-            }
-          })
-        end)
-      vim.keymap.set('n', ';r', function()
-        builtin.live_grep()
-      end)
-      vim.keymap.set('n', '\\\\', function()
-        builtin.buffers({
-          sort_mru = true,
-          sort_lastused = true,
-          initial_mode = "normal",
-        })
-      end)
-      vim.keymap.set('n', ';t', function()
-        builtin.help_tags()
-      end)
-      vim.keymap.set('n', ';;', function()
-        builtin.resume()
-      end)
-      vim.keymap.set('n', ';e', function()
-        builtin.diagnostics()
-      end)
     end
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   }
 }
