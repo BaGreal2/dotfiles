cfa8e7636190316d6d3d26a9a292648e025c02c0
BaGreal2 <artemonik17@gmail.com>
2025-05-24 00:37:52 +0200

    [nvim] update colorscheme to zenbones

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,35 @@
 return {
-  -- {
-  --   'p00f/alabaster.nvim',
-  --   config = function()
-  --     -- vim.g.alabaster_dim_comments = true
-  --     vim.cmd('colorscheme alabaster')
-  --
-  --     vim.api.nvim_set_hl(0, "Normal", { bg = nil })
-  --     vim.api.nvim_set_hl(0, "NonText", { bg = nil })
-  --     vim.api.nvim_set_hl(0, "CursorLine", { bg = "#202020" })
-  --   end,
-  -- },
+  {
+    'p00f/alabaster.nvim',
+    enabled = false,
+    config = function()
+      -- vim.g.alabaster_dim_comments = true
+      vim.cmd('colorscheme alabaster')
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = nil })
+      vim.api.nvim_set_hl(0, "NonText", { bg = nil })
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#202020" })
+    end,
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.g.zenbones_darken_comments = 45
+      vim.g.zenbones = {
+        transparent_background = true
+      }
+      vim.g.zenwritten = {
+        transparent_background = true
+      }
+      vim.cmd.colorscheme('zenbones')
+    end
+  },
   {
     'rebelot/kanagawa.nvim',
+    enabled = false,
     config = function()
       require('kanagawa').setup({
         compile = true,
