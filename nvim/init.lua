c8823ae68309d2c7b8b0395f42d653e14425fca9
BaGreal2 <artemonik17@gmail.com>
2025-05-24 00:40:16 +0200

    [nvim] add autocmds to config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require('highlights')
 require('maps')
 require('plugin-setup')
 require('commands')
+require('autocmds')
 
 -- Highlights linking (Needs to be here before initial draw)
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
