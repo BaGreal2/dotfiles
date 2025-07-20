07622188e09d344436c3d9c2a4834a967e358795
BaGreal2 <artemonik17@gmail.com>
2025-07-20 13:56:10 +0200

    [newsboat] open images

--- newsboat/open.sh
+++ newsboat/open.sh
@@ -4,6 +4,13 @@ case "$1" in
     *youtube.com*|*youtu.be*)
         iina "$1" --mpv-fs
         ;;
+    *.png*|*.jpg*|*.jpeg*|*.gif*|*.webp*)
+        clean_url="${1%%\?*}"
+        ext="${clean_url##*.}"
+        tmpfile=$(mktemp "/tmp/img.XXXXXX.$ext")
+        trap 'rm -f "$tmpfile"' EXIT
+        curl -s -L -v "$1" -o "$tmpfile" && /Applications/qView.app/Contents/MacOS/qView "$tmpfile"
+        ;;
     *)
         librewolf "$1"
         ;;
