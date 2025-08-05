cdfe424de6ca160ffceba2b845a64d65f447dbfe
BaGreal2 <artemonik17@gmail.com>
2025-08-05 02:02:55 +0200

    [nvim] don't lazyload treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,23 +3,16 @@ return {
     'nvim-treesitter/nvim-treesitter',
     version = false,
     build = ':TSUpdate',
-    event = { "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0,
-    init = function(plugin)
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
+    lazy = false,
+    priority = 1000,
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "c", "cpp", "lua", "python", "rust", "typescript", "javascript", "tsx", "html", "css", "diff", "markdown", "markdown_inline", "luadoc", "xml" },
+        ensure_installed = { "c", "cpp", "lua", "python", "rust", "typescript", "javascript", "tsx", "html", "css", "diff", "markdown", "markdown_inline", "luadoc" },
         highlight = {
           enable = true,
         },
       })
-
-      vim.treesitter.language.register('xml', 'axaml')
-      -- vim.treesitter.language.register('tsx', 'javascript')
     end,
   },
 }
