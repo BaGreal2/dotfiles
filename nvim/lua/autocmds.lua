0b517cabe14ed92281be3205d1c81b13c1398e60
BaGreal2 <artemonik17@gmail.com>
2025-08-05 02:04:39 +0200

    [nvim] remove unused axaml file handling

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "typescript", "typescriptreact" },
   callback = function()
     vim.cmd("compiler tsc")
-    local project_dir = vim.fn.findfile("tsconfig.json", ".;") -- searches upward
+    local project_dir = vim.fn.findfile("tsconfig.json", ".;")
     if project_dir ~= "" then
       local tsconfig_path = vim.fn.fnamemodify(project_dir, ":p")
       vim.opt_local.makeprg = "npx tsc --noEmit --pretty false --project " .. tsconfig_path
@@ -13,9 +13,4 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
   group = tsc_comp_group,
-})
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.axaml",
-  callback = function() vim.bo.filetype = "axaml" end,
 })
