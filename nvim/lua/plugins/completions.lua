6d603dbbef18716273cdc5d8c90e2b9d6db36108
BaGreal2 <artemonik17@gmail.com>
2025-08-05 02:04:15 +0200

    [nvim] switch to blink.cmp for completions

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,89 +1,30 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-nvim-lsp',
+    'saghen/blink.cmp',
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'enter',
+        ['<C-space>'] = false,
+        ['<C-n>'] = {
+          'show',
+          'select_next'
+        }
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+        },
+        menu = {
+          auto_show = false,
+          border = "rounded",
+          winhighlight =
+          "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+        }
+      },
     },
-    config = function()
-      -- local lspkind = require 'lspkind'
-      local icons = require('mini.icons')
-      icons.setup()
-
-      require('cmp').setup({
-        completion = {
-          autocomplete = false,
-        },
-        mapping = require('cmp').mapping.preset.insert({
-          ['<C-d>'] = require('cmp').mapping.scroll_docs(-4),
-          ['<C-f>'] = require('cmp').mapping.scroll_docs(4),
-          ['<C-Space>'] = require('cmp').mapping.complete(),
-          ['<C-e>'] = require('cmp').mapping.close(),
-          ['<CR>'] = require('cmp').mapping.confirm({
-            behavior = require('cmp').ConfirmBehavior.Replace,
-            select = true,
-          }),
-        }),
-        sources = require('cmp').config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-        }),
-        -- formatting = {
-        --   format = lspkind.cmp_format({
-        --     with_text = false,
-        --     maxwidth = 50,
-        --   }),
-        -- },
-        formatting = {
-          fields = { 'abbr', 'menu' },
-          format = function(entry, item)
-            local icon, hl = icons.get('lsp', item.kind)
-            if icon then
-              item.menu = icon .. ' '
-              item.menu_hl_group = hl
-            end
-            return item
-          end,
-        },
-        window = {
-          completion = require('cmp').config.window.bordered(),
-          documentation = require('cmp').config.window.bordered(),
-        },
-      })
-
-      vim.cmd [[
-        set completeopt=menuone,noinsert,noselect
-        highlight! default link CmpItemKind CmpItemMenuDefault
-      ]]
-    end,
+    opts_extend = { "sources.default" }
   },
-  -- {
-  --   'jose-elias-alvarez/null-ls.nvim',
-  --   event = 'BufReadPre',
-  --   config = function()
-  --     require('null-ls').setup({
-  --       sources = {
-  --         -- require('null-ls').builtins.formatting.prettierd,
-  --         -- require('null-ls').builtins.diagnostics.fish
-  --       }
-  --     })
-  --   end,
-  -- },
-  {
-    'numToStr/Comment.nvim',
-    event = "VeryLazy",
-    config = function()
-      require('Comment').setup({
-        toggler = {
-          line = 'mm',
-          block = 'MM'
-        },
-        opleader = {
-          line = 'mm',
-          block = 'MM',
-        },
-      })
-    end,
-  }
 }
