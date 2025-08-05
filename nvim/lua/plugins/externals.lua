62ac936e1f3c6352e2cefe8273e2227e7561f0e0
BaGreal2 <artemonik17@gmail.com>
2025-08-05 02:44:25 +0200

    [nvim] remove peek.nvim

--- nvim/lua/plugins/externals.lua
+++ nvim/lua/plugins/externals.lua
@@ -21,14 +21,4 @@ return {
       })
     end
   },
-  {
-    "toppair/peek.nvim",
-    ft = { "markdown" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
 }
