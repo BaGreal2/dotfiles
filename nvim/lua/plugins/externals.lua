f845b345a7fa0eef582e469cef1ff497ae0ed418
BaGreal2 <artemonik17@gmail.com>
2025-08-08 20:06:30 +0200

    [nvim] switch to Codeium

--- nvim/lua/plugins/externals.lua
+++ nvim/lua/plugins/externals.lua
@@ -1,8 +1,9 @@
 return {
   {
     'zbirenbaum/copilot.lua',
+    cmd = "Copilot",
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "scss" },
-    cmd = { "Copilot" },
+    enabled = false,
     config = function()
       local on_attach = function(client, bufnr)
         vim.api.nvim_create_autocmd("VimEnter", {
@@ -21,4 +22,19 @@ return {
       })
     end
   },
+  {
+    'Exafunction/windsurf.vim',
+    cmd = "Codeium",
+    ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "scss" },
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      vim.keymap.set('i', '<A-l>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<A-w>', function() return vim.fn['codeium#AcceptNextWord']() end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<A-.>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<A-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+    end
+  }
 }
