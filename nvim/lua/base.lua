e3ba17cece55c14b7c7662308ad2f5cd46c5e99a
BaGreal2 <artemonik17@gmail.com>
2025-08-13 22:07:34 +0200

    [nvim] remove unused base vim settings

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,7 +1,3 @@
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -10,22 +6,16 @@ vim.opt.undofile = true
 vim.opt.swapfile = false
 vim.opt.hlsearch = true
 vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.shell = 'fish'
-vim.opt.backupskip = '/tmp/*,/private/tmp/*'
 vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.ai = true
-vim.opt.si = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
