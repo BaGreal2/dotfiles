174256ef55367d5ca8a3d507267d4d13fc66a7a8
BaGreal2 <artemonik17@gmail.com>
2025-08-13 10:34:51 +0200

    [nvim] remove toggle terminal

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -14,32 +14,3 @@ keymap.set('n', '<leader>k', ':wincmd K<CR>', { noremap = true, silent = true })
 keymap.set('n', '<leader>j', ':wincmd J<CR>', { noremap = true, silent = true })
 
 keymap.set('n', '<M-k>', 'q:i', { silent = true })
-
-local terminal_bufnr = nil
-local terminal_winid = nil
-
-function _G.toggle_terminal()
-  if terminal_winid and vim.api.nvim_win_is_valid(terminal_winid) then
-    vim.api.nvim_win_close(terminal_winid, true)
-    terminal_winid = nil
-    return
-  end
-
-  if terminal_bufnr and vim.api.nvim_buf_is_valid(terminal_bufnr) then
-    vim.cmd.vnew()
-    terminal_winid = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(terminal_winid, terminal_bufnr)
-  else
-    vim.cmd.vnew()
-    terminal_winid = vim.api.nvim_get_current_win()
-    vim.cmd.term()
-    terminal_bufnr = vim.api.nvim_get_current_buf()
-  end
-
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
-  vim.cmd.startinsert()
-end
-
-vim.keymap.set('n', '<leader>st', toggle_terminal)
-vim.keymap.set('t', '<leader>st', [[<C-\><C-n>:lua toggle_terminal()<CR>]], { noremap = true, silent = true })
