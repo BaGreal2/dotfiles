77d21b0daa19be40e1f8f9d84c32d37f2e219d36
BaGreal2 <artemonik17@gmail.com>
2025-08-13 10:35:39 +0200

    [nvim] simplify highlights config

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -1,7 +1,6 @@
-vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
 vim.opt.winborder = 'rounded'
-vim.opt.pumblend = 0
 vim.opt.background = 'dark'
+
+vim.g.markdown_fenced_languages = { 'html', 'python', 'lua', 'vim', 'typescript', 'javascript', 'typescriptreact', 'sh',
+  'bash', 'c', 'cpp', 'rust' }
