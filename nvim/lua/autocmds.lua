cc7a2a0887939e4bed153e3c064e4585d60ee397
BaGreal2 <artemonik17@gmail.com>
2025-08-13 10:35:32 +0200

    [nvim] make setup for rust

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -14,3 +14,30 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
   group = tsc_comp_group,
 })
+
+local rust_comp_group = vim.api.nvim_create_augroup("rust_comp", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  callback = function()
+    -- pcall(vim.cmd, "compiler cargo")
+    vim.cmd("compiler cargo")
+
+    vim.opt_local.makeprg = "cargo run --color never"
+
+    if vim.bo.efm == "" or vim.bo.efm:match("rust") == nil then
+      vim.bo.errorformat = table.concat({
+        '%Eerror: %m',
+        '%Eerror[%t%*[^]]]: %m',
+        '%Wwarning: %m',
+        '%Wwarning[%t%*[^]]]: %m',
+        '%Inote: %m',
+        '%C%*[^-]--> %f:%l:%c',
+        '%C%\\s%f:%l:%c',
+        '%C%\\s= note: %m',
+        '%Z'
+      }, ",")
+    end
+  end,
+  group = rust_comp_group,
+})
