8815089ed301e380007f372dc452eed97615c709
BaGreal2 <artemonik17@gmail.com>
2025-08-13 10:33:21 +0200

    [nvim] switch to windsurf

--- nvim/lua/plugins/externals.lua
+++ nvim/lua/plugins/externals.lua
@@ -1,28 +1,5 @@
 return {
   {
-    'zbirenbaum/copilot.lua',
-    cmd = "Copilot",
-    ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "scss" },
-    enabled = false,
-    config = function()
-      local on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("VimEnter", {
-          callback = function()
-            vim.cmd("Copilot")
-          end,
-        })
-      end
-
-      require('copilot').setup({
-        on_attach = on_attach,
-        suggestion = {
-          auto_trigger = true,
-          hide_during_completion = false
-        }
-      })
-    end
-  },
-  {
     'Exafunction/windsurf.vim',
     cmd = "Codeium",
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "scss" },
