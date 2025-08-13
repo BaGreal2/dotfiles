65a24d7770311b7ba3c3cfeb94d122efe2af38ae
BaGreal2 <artemonik17@gmail.com>
2025-08-13 22:06:24 +0200

    [nvim] maps for quickfix list jump

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -8,9 +8,7 @@ keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
 
-keymap.set('n', '<leader>h', ':wincmd H<CR>', { noremap = true, silent = true })
-keymap.set('n', '<leader>l', ':wincmd L<CR>', { noremap = true, silent = true })
-keymap.set('n', '<leader>k', ':wincmd K<CR>', { noremap = true, silent = true })
-keymap.set('n', '<leader>j', ':wincmd J<CR>', { noremap = true, silent = true })
-
 keymap.set('n', '<M-k>', 'q:i', { silent = true })
+
+keymap.set('n', '<C-n>', '<cmd>cnext<cr>')
+keymap.set('n', '<C-p>', '<cmd>cprevious<cr>')
